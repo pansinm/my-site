e727cebfd0eb95b631fb14c93f6245ce18d2c124
潘昕 <pansinm@foxmail.com>
2019-12-09 14:52:27 +0800

    refactor: 抽取gatsby-plugin-tajs作为本地插件

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   plugins: [
     {
@@ -10,11 +12,18 @@ module.exports = {
     // with gatsby-plugin-theme-ui, the last theme in the config
     // will override the theme-ui context from other themes
     { resolve: `gatsby-theme-blog` },
+    {
+      resolve: 'gatsby-plugin-tajs',
+      options: {
+        sid: process.env.TAJS_SID
+      }
+    }
   ],
   siteMetadata: {
     title: `小橙时光机`,
     author: 'pansinm',
     description: '千里之行始于足下',
     social: []
-  },
+  }
 }
+console.log(process.env);
--- plugins/gatsby-plugin-tajs/gatsby-ssr.js
+++ plugins/gatsby-plugin-tajs/gatsby-ssr.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 // 插入站点统计代码
-export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }, options) => {
+  console.log(options);
   if (process.env.NODE_ENV === 'production') {
     const headComponents = getHeadComponents();
     headComponents.push(
       <script
         type="text/javascript"
-        src={`http://tajs.qq.com/stats?sId=${process.env.TAJS_SID}`}
+        src={`http://tajs.qq.com/stats?sId=${options.sid}`}
         charset="UTF-8"
       />
     )
