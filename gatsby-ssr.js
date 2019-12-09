a304ca893e8c6f05030d0994e85ba13e8a948c72
潘昕 <pansinm@foxmail.com>
2019-12-09 13:57:13 +0800

    feat: 接入站点统计

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,14 @@
-const React = require('react');
+import React from 'react';
 
-exports.onRenderBody = ({ setHeadComponents }) => {
-  // if (process.env.NODE_ENV === `production`) {
-    const TAJA_ANALYTICS_SCRIPT = (
-      <script
-        type="text/javascript"
-        src={`http://tajs.qq.com/stats?sId=${process.NODE_ENV.TAJS_SID}`}
-        charset="UTF-8"
-      />
-    );
-    console.log(TAJS_ANA2LYTICS_SCRIPT)
-    setHeadComponents([TAJA_ANALYTICS_SCRIPT]);
-  // }
-  // return null;
+// 插入站点统计代码
+export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  headComponents.push(
+    <script
+      type="text/javascript"
+      src={`http://tajs.qq.com/stats?sId=${process.NODE_ENV.TAJS_SID}`}
+      charset="UTF-8"
+    />
+  )
+  replaceHeadComponents(headComponents);
 };
