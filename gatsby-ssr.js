8013c3c52f586d75983cd4cb9fec991835a3f466
潘昕 <pansinm@foxmail.com>
2019-12-09 14:27:16 +0800

    feat: production环境才插入站点统计代码

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 // 插入站点统计代码
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-  const headComponents = getHeadComponents();
-  headComponents.push(
-    <script
-      type="text/javascript"
-      src={`http://tajs.qq.com/stats?sId=${process.NODE_ENV.TAJS_SID}`}
-      charset="UTF-8"
-    />
-  )
-  replaceHeadComponents(headComponents);
+  if (process.env.NODE_ENV === 'production') {
+    const headComponents = getHeadComponents();
+    headComponents.push(
+      <script
+        type="text/javascript"
+        src={`http://tajs.qq.com/stats?sId=${process.env.TAJS_SID}`}
+        charset="UTF-8"
+      />
+    )
+    replaceHeadComponents(headComponents);
+  }
 };
